a4f23f4f2a1839ac96a8f7edca20b466a32f6aea
Corentin384 <cocotex38@gmail.com>
2022-03-23 13:24:55 +0100

    testing field "Première adresse IP" if is a IP address

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
 function forgetMDP() {
-    alert("Login : jsp"+"\n"+"Mot de passe : 1234");
+    alert("Login : jsp \n Mot de passe : 1234");
+}
+
+function retourMenu(){
+    document.location.href="../templates/index.html"
+}
+
+function ValidateIPaddress(ipaddress) {
+    let patternIp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if (patternIp.test(ipaddress)) {
+        return true
+    }else{
+        alert("You have entered an invalid IP address!")
+        return false
+    }
 }
